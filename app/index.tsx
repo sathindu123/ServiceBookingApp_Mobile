55f0d16381155d4d556d1b07c179fb54e4bd1a6c
sathindu123 <sathindusathsarakumara@gmail.com>
2026-01-30 09:13:59 +0530

    init project

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { View, Text } from 'react-native-reanimated/lib/typescript/Animated'
+import "./global.css"
+
+function index() {
+  return (
+    <View> <Text> bmko </Text> </View>
+  )
+}
+
+export default index
